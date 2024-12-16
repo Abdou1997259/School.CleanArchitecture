190f55d5c6cb18495b89ad04bcc4324a54ac587f
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-12-16 11:15:15 +0200

    Add project files.

---
+++ School.Data/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Data.Entities
+{
+    public class Student
+    {
+
+        [Key]
+        public int StudId { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Address { get; set; }
+        [StringLength(500)]
+        public string Phone { get; set; }
+
+        public int? DID { get; set; }
+        [ForeignKey(nameof(DID))]
+        [InverseProperty("Students")]
+        public virtual Department Department { get; set; } = default!;
+
+
+
+    }
+}
---
+++ School.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Data.Entities;
+
+namespace School.Infrastructure.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+
+    }
+}
